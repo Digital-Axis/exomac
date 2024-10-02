cfec51518fb772541a8658a2cb69b1a4551fcce9
Digital-Axis <info@digi-axis.com>
2024-10-02 09:55:28 +0200

    Update GoogleMap.js

--- src/container/Map/GoogleMap.js
+++ src/container/Map/GoogleMap.js
@@ -5,8 +5,8 @@ const GoogleMap = () => {
         <div className="google-map-area section text-center section-padding-bottom">
             <div className="container">
                 <div className="contact-map-area" data-aos="fade-up">
-                    <iframe className="contact-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2136.986005919501!2d-73.9685579655238!3d40.75862446708152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c258e4a1c884e5%3A0x24fe1071086b36d5!2sThe%20Atrium!5e0!3m2!1sen!2sbd!4v1585132512970!5m2!1sen!2sbd"  aria-hidden="false"></iframe>
-                </div>
+                    <iframe className="contact-map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3846.558177851111!2d28.2914644!3d-15.4003963!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1940f52945308c7f%3A0x88f761c944ef747!2sPerfect%20signage%20Marketing%20Limited!5e0!3m2!1sen!2szm!4v1727855584049!5m2!1sen!2szm"  aria-hidden="false"></iframe>
+                </div>                               
             </div>
         </div>
     )
