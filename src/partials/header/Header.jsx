8a94a0efddbc7064fedc2eade8080386d5c63805
Digital-Axis <info@digi-axis.com>
2026-03-17 10:19:52 +0000

    Start Project

--- src/partials/header/Header.jsx
+++ src/partials/header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
                             </div>
 
                             <div className="col-xl-2 col d-none d-sm-flex justify-content-end order-1 order-xl-2">
-                                <Btn name='Start Your Project' />
+                                <Btn name='Start Project' />
                             </div>
                         </div>
                     </div>
