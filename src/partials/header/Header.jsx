177a182ec6d585cfc68623371f7d785967be4d4d
Digital-Axis <info@digi-axis.com>
2026-03-17 09:16:26 +0000

    Consultation

--- src/partials/header/Header.jsx
+++ src/partials/header/Header.jsx
@@ -62,7 +62,7 @@ const Header = () => {
                             </div>
 
                             <div className="col-xl-2 col d-none d-sm-flex justify-content-end order-1 order-xl-2">
-                                <Btn name='Book a Consultation' />
+                                <Btn name='Consultation' />
                             </div>
                         </div>
                     </div>
