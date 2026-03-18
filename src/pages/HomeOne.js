4e47a07325fe3464d1b91038e00a43e92bced653
Digital-Axis <info@digi-axis.com>
2026-03-18 08:43:33 +0000

    Home minimize

--- src/pages/HomeOne.js
+++ src/pages/HomeOne.js
@@ -2,10 +2,10 @@ import React from 'react';
 import SEO from '../components/SEO';
 import Header from "../partials/header/Header";
 import IntroSlider from '../container/IntroSlider/IntroSlider';
-import HomeAbout from '../components/About/HomeAbout.jsx';
-import ServiceIconBox from '../container/service/ServiceIconBox';
-import HomeSuccess from '../components/Success/HomeSuccess';
-import Portfolio from '../container/Portfolio/Portfolio';
+//import HomeAbout from '../components/About/HomeAbout.jsx';
+//import ServiceIconBox from '../container/service/ServiceIconBox';
+//import HomeSuccess from '../components/Success/HomeSuccess';
+//import Portfolio from '../container/Portfolio/Portfolio';
 //import HomeBlog from '../container/BlogGrid/HomeBlog';
 //import Newsletter from '../container/Newsletter/Newsletter';
 import ContactInformation from '../container/ContactInformation/ContactInformation';
@@ -22,13 +22,11 @@ const HomeOne = () => {
             <SEO title="Perfect Signage Marketing Ltd" />
             <Header />
             <IntroSlider />
-            <HomeAbout />
-            <ServiceIconBox classOption="bg-color-1" />
-            <HomeSuccess />
-            <Portfolio />
+            <ContactInformation />
+            
             <Video />
             
-            <ContactInformation />
+           
             <BrandContainer classOption="section-padding-bottom" />
             <Footer />
             <ScrollToTop />
